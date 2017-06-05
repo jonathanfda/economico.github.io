0b84ff674285321d607b2ddd6bf28339ca7aad6d
jonathanfda <jonathan.fer.de@gmail.com>
2017-06-05 14:29:30 +0100

    economico
    pagina web plan economico ejemplo

---
+++ index.php
@@ -0,0 +1,507 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="description" content="Diseñamos y comercializamos mapas turísticos, prestamos servicios de diseño gráfico, diseño web, fotografía e imprenta en Tenerife.  ">
+<meta name="author" content="jonathan@masquemapas.com">
+<meta name="robots" content="noindex, nofollow">
+<link rel="icon" href="img/favicon.png" type="image/png" />
+<title>M&aacute;s que Mapas</title>
+<!-- Bootstrap Core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans" rel="stylesheet">
+<!-- Plugin CSS -->
+<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+<!-- Theme CSS -->
+<link href="css/creative.css" rel="stylesheet">
+<script src="js/gen_validatorv4.js"></script>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<style>
+body{font-family: 'Quattrocento Sans', sans-serif;
+margin: 0 auto;width:100%; background:#333}
+	header{min-height:570px}
+h2{margin-bottom:8px; margin-top:8px}
+h2.titulos{font-size:25px; font-weight:900; text-align:center; color:#000}
+h1#homeHeading{text-align:right; margin-bottom:0px; margin-top:0px}
+.header{background: url(img/restaurante-2.jpg) center center no-repeat ; background-size:cover; position:relative}
+.text-muted{color:#555}
+.fa-4x{font-size:40px}
+img.logo.sr-contact{margin:15px 0}
+.principal{margin-top:-150px; background:white}
+.contacto-header{padding:10px 10px; background:transparent; color:white; font-size:18px; max-width:300px; width:100%; position:absolute;right:0; text-align:left;  bottom:0}
+.comunicate{font-size:24px; color:#222; display:block }
+a.reserva{background:rgba(0, 0, 0, 0.8); border:2px solid #fff; color:white; padding:10px 20px; text-decoration:none; font-size:16px}
+a.reserva:hover{background:rgba(255, 255, 255, 0.5); border:2px solid #232a82; color:#232a82; padding:10px 20px;}}
+   input::-moz-placeholder {
+color: #333;
+}
+
+
+.formulario-izquierda{width:45%; float:left}
+.formulario-derecha{width:55%; float:right; padding-left:5px}
+input:-moz-placeholder {
+color: #333;
+}
+.form-control {
+display: block;
+width: 100%;
+height: 50px;
+font-size: 18px;
+line-height: 1.42857143;
+color: #555;
+background-color: #eee;
+background-image: none;
+border: 0;
+border-radius: 0;
+-webkit-box-shadow: none;
+box-shadow: none;
+-webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
+-o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;}
+
+
+textarea.form-control{height:120px}
+	
+input.form-control{height:63px}
+.form-group{margin-bottom:5px}
+.form-control::-webkit-input-placeholder { color: #444; }
+.form-control:-moz-placeholder { color: #444; }
+.form-control::-moz-placeholder { color: #444; }
+.form-control:-ms-input-placeholder { color: #444; }
+textarea.form-control{height:200px}
+h1, h2, h3, h4{font-weight:normal}
+.portfolio-box .portfolio-box-caption {
+color: white;
+opacity: 0;
+display: block;
+background: rgba(103, 133, 201, 0.35)}
+.portfolio-box .portfolio-box-caption .portfolio-box-caption-content .project-category{font-size:24px !important}
+
+ 
+h3.sr-icons{font-size:24px !important}
+
+.fa-4x {
+font-size: 24px;
+margin-right: 20px;
+}
+p{color:#333;font-size:17px; text-align:justify}
+.img-responsive{margin:0 auto}
+#barraaceptacion {
+display:none;
+position:fixed;
+left:0px;
+right:0px;
+bottom: 0px;
+padding-bottom: 0;
+width: 100%;
+text-align: center;
+min-height: 80px;
+background-color: rgba(0, 0, 0, 0.5);
+color:#fff;
+z-index:99999;
+}
+ 
+.inner {
+width:100%;
+position:absolute;
+font-size:15px;
+top:10%;
+}
+ 
+.inner a.ok {
+padding:4px;
+color:#00ff2e;
+text-decoration:none;
+}
+ 
+.inner a.info {
+padding-left:5px;
+text-decoration:none;
+color:#faff00;
+}
+.iconos{font-size:30px}
+
+span.contacto-home{display:inline-block}
+.botones-redes{
+	border: 1px solid white;
+width: 40px;
+height: 40px;
+text-align: center;
+padding: 10px;
+border-radius: 90px;
+}
+.botones-redes:hover{
+border: 1px solid #333333;
+width: 40px;
+height: 40px;
+text-align: center;
+padding: 10px;
+border-radius: 90px;
+color:#333333
+}
+
+@media screen and (min-width:1440px)
+{
+header{min-height:650px}
+}
+input.form-control{color:#555}
+button.btn.btn-default.contacto{color:white;background-color: rgb(51, 51, 51);  border: 2px solid #333;  padding: 10px 20px; font-size:16px; font-weight:normal}
+button.btn.btn-default.contacto:hover{color:#292929;background-color:#fff}
+@media (min-width: 992px){
+.col-md-offset-3 {
+margin-left: 23.7%;
+}}
+.contenedor-presentacion{padding:12% 0 0 0}
+
+@media screen and (min-width:1680px)
+{
+	body{max-width:1800px; width:100%; margin:0 auto}
+
+header{min-height:750px}
+.contenedor-presentacion{padding:13% 0 0 0}
+
+}
+@media screen and (min-width:1900px)
+{
+header{min-height:850px}
+.contenedor-presentacion{padding:15% 0 0 0}
+
+}
+@media screen and (min-width:2000px)
+{
+header{min-height:950px}
+.contenedor-presentacion{padding:18% 0 0 0}
+
+}
+
+@media screen and (max-width:1024px){
+.header{ background-size:cover; position:relative; min-height:650px}
+.contacto-header{padding:20px 10px; background:transparent; color:white; font-size:18px; max-width:100%; width:100%; position:absolute;right:0; text-align:center;  bottom:0}
+.col-md-2{width:33%; display:inline-block}
+.iconos{font-size:25px}
+}
+
+@media screen and (max-width:768px)
+{
+.header{ background-size:cover; position:relative; min-height:530px}
+.contenedor-presentacion{padding:80px 0 0 0}
+.contacto-header{width:100%; max-width:100%; text-align:center; right:initial}
+.col-md-3{width:49%; display:inline-block; padding:20px}
+.contacto-header{padding:20px 10px; background:transparent; color:white; font-size:18px; max-width:100%; width:100%; position:absolute;right:0; text-align:center;  bottom:0}
+.col-md-2{width:33%; display:inline-block}
+.iconos{font-size:25px}
+.como-llegar{padding-top:30px}
+}
+
+
+@media screen and (max-width:736px){
+	.header{min-height:500px}
+
+}
+
+@media screen and (max-width:736px){
+	.header{min-height:460px}
+
+}
+
+@media screen and (max-width:568px){
+	.header{min-height:420px}
+.formulario-izquierda{width:100%; float:none; display:block}
+.formulario-derecha{width:100%; float:none; display:block; padding-left:0px}
+}
+
+@media screen and (max-width:540px){
+.col-md-2{width:49%; display:inline-block}
+.principal{margin-top:-80px; background:white}
+button.btn.btn-default.contacto{width:100%}
+
+}
+
+@media screen and (max-width:460px){
+#barraaceptacion{top:0; bottom:initial; min-height:70px}
+.inner {
+	line-height:14px;
+width: 100%;
+position: absolute;
+padding-left: 5px;
+font-size: 12px;
+top: 10%;}
+.header{ background-size:cover; position:relative; min-height:650px}
+.contenedor-presentacion{padding:100px 0 0 0}
+.contacto-header{padding:5px 10px; background:transparent; color:white; font-size:16px; max-width:100%; position:absolute; bottom:0; text-align: center;width: 100%;}
+.comunicate{font-size:20px}
+span.contacto-home{display:block; font-size:18px;}
+@media screen and (max-width:400px){
+	.header{background-size:cover;  min-height:500px}
+
+}
+
+}
+@media screen and (max-width:320px){
+	.header{background: url(img/masquemapas-bg-movil.jpg) top center no-repeat #0587d2;background-size:cover;  min-height:460px}
+
+}
+</style><!-- This closes the open style tag above. Just a jsFiddle thing. Please ignore. -->
+</head>
+<body id="page-top">
+<header class="header">
+				<?php 
+	 $msj = isset($_GET['msj']) ? $_GET['msj'] : NULL;
+
+	if ($msj == 'send'){
+		echo '<div style="text-align:center; padding:2px; background:rgba(2, 128, 0, 0.51); color:white; text-transform:uppercase">
+		Gracias por contactarnos, nos comunicaremos contigo lo antes posible!
+		</div>';
+	} if ($msj == 'error'){
+		echo '<div style="text-align:center; padding:2px 0px; background:rgba(241, 0, 77, 0.61); color:white;  text-transform:uppercase">
+El mensaje no se ha podido enviar. Intenta m&aacute;s tarde!
+</div>';
+	}
+	?>
+<div style="position:absolute; top:10%; text-align:center; width:100%">
+<h1><img src="img/logo-bahia-los-abrigos.png" style="width:200px"/></h1>
+<div style="text-align:center;  margin-top:50px">
+<a href="#contact" class="reserva page-scroll">RESERVE AHORA</a>
+</div>
+</div>
+</header>
+	<section style="padding:20px; background:#fff">
+	<div class="sr-icons" style=" padding:20px 0; ">
+  <div class="container principal">
+<h2 style="text-align:center; padding:30px 0">EL RESTAURANTE</h2>
+<div class="row">
+<p style="padding:0 40px">
+Restaurante Bahía Los Abrigos le ofrece su especialidad en Pescado Fresco a la Sal único en la localidad de Los Abrigos en Tenerife Sur. Así mismo usted podrá degustar todo tipo de productos procedentes del mar, tales como Parrilladas de Mariscos (navajas, ostras, mejillones, almejas, langostas, bogavantes, carabineros) y de Pescado. De manera muy especial, le ofrecemos una amplia oferta de Arroces (arroz caldoso, fideuá, paella de mariscos, spaguetti marinera). 
+ <br><br>
+Contamos con una terraza chillout en la planta alta donde podrá tomar una copa con unas vistas privilegiadas de la bahía de Los Abrigos.
+</p>
+</div>
+</div>
+</div>
+</section>	
+<section id="services" style="background:#eee; padding:40px 0 80px 0">
+	<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<!--  <h2 class="section-heading  sr-icons" style="color:#333; padding-bottom:0px">T&Iacute;ITULO</h2>-->
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<!--  SERVICIO 1 -->
+<div class="col-md-4 text-left">
+<div class="service-box sr-icons">
+<img src="img/terraza.jpg" class="img-responsive"/>
+<div style="background:#b1b9c4; padding:20px 20px 30px">
+<h3  style="color:#332f2e; padding:20px 0; text-align:center; font-size:20px; margin:0; text-transform:initial">LA TERRAZA</h3>
+<p class="text-muted" style="color:#111;">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco 
+</p>
+  </div>
+</div>
+</div>
+				<!--  FIN SERVICIO 1 -->
+				<!--  SERVICIO 2 -->
+
+<div class="col-md-4 text-left">
+  <div class="service-box sr-icons">
+					
+		<img src="img/instalaciones.jpg" class="img-responsive"/>
+				<div style="background:#333; padding:20px 20px 30px">
+
+		<h3  style="color:#fff; padding:20px 0; text-align:center; font-size:20px; margin:0; text-transform:initial">INSTALACIONES</h3>
+
+						<p class="text-muted" style="color:#fff">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco 
+						</p>
+
+  </div>
+  </div>
+</div>
+				
+				<!--  FIN SERVICIO 2 -->
+				<!--  SERVICIO 3 -->
+<div class="col-md-4 text-left">
+  <div class="service-box sr-icons">
+ <img src="img/reserve.jpg" class="img-responsive"/>
+ 				<div style="background:#fff; padding:20px 20px 30px">
+			<h3  style="color:#332f2e; padding:20px 0; text-align:center; font-size:20px; margin:0; text-transform:initial">RESERVACIONES</h3>
+
+<p class="text-muted" style="display:block">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.<br><br></p>
+<p style="text-align:center"><a href="#contact" class="reserva page-scroll">RESERVE AHORA</a></p>
+
+
+			</div>
+			</div>				
+			</div>				
+				
+				<!--  SERVICIO 3 -->
+				<div class="clearfix"></div>
+
+</div>
+  </div>
+</section>
+
+<section class="no-padding" id="portfolio">
+  <div class="container-fluid">
+<div class="row no-gutter popup-gallery">
+ 
+<div class="col-lg-4 col-sm-6">
+  <a href="img/galeria-2.jpg" class="portfolio-box">
+<img src="img/galeria-2.jpg" class="img-responsive" alt="">
+<div class="portfolio-box-caption">
+  
+</div>
+  </a>
+</div>
+<div class="col-lg-4 col-sm-6">
+  <a href="img/galeria-6.jpg" class="portfolio-box">
+<img src="img/galeria-6.jpg" class="img-responsive" alt="">
+<div class="portfolio-box-caption">
+
+</div>
+  </a>
+</div>
+   <div class="col-lg-4 col-sm-6">
+  <a href="img/galeria-1.jpg" class="portfolio-box">
+<img src="img/galeria-1.jpg" class="img-responsive" alt="Servicios Web Maacute;s que Mapas">
+<div class="portfolio-box-caption">
+  
+</div>
+  </a>
+</div>
+
+<div class="col-lg-4 col-sm-6">
+  <a href="img/galeria-3.jpg" class="portfolio-box">
+<img src="img/galeria-3.jpg" class="img-responsive" alt="">
+<div class="portfolio-box-caption">
+ 
+</div>
+  </a>
+</div>
+<div class="col-lg-4 col-sm-6">
+  <a href="img/galeria-5.jpg" class="portfolio-box">
+<img src="img/galeria-5.jpg" class="img-responsive" alt="">
+<div class="portfolio-box-caption">
+ 
+</div>
+  </a>
+</div>
+				 <div class="col-lg-4 col-sm-6">
+  <a href="img/galeria-4.jpg" class="portfolio-box">
+<img src="img/galeria-4.jpg" class="img-responsive" alt="">
+<div class="portfolio-box-caption">
+  
+</div>
+  </a>
+</div>
+
+</div>
+  </div>
+</section>
+
+
+
+<section id="contact" style="background:#fff; padding:50px 0">
+  <div class="container">
+<div class="row">
+	<div class="col-md-8 text-center">
+<h4 style="text-align:left; font-size:22px; ">ESCRIBANOS Y RESERVE</h4>
+ <form action="#" method="post" name="formulario">
+   <div class="formulario-izquierda">
+
+	<div class="form-group">
+<input type="text" class="form-control"  placeholder="Nombre" name="name">
+<input type="hidden" />
+  </div>
+   <div class="form-group">
+<input type="email" class="form-control"  placeholder="Email" name="email">
+  </div>
+   <div class="form-group">
+<input type="text" class="form-control" placeholder="Teléfono" name="subject">
+  </div>
+ 
+  </div> 
+  <div class="formulario-derecha">
+
+  <div class="form-group">
+<textarea  class="form-control"  placeholder="Mensaje restaurante / N&uacute;mero de personas / fecha y hora de reserva " name="message"></textarea>
+  </div>
+ </div>
+   <div class="form-group" style="text-align:left">
+  <button type="submit" class="btn btn-default contacto">ENVIAR</button>
+</div>  
+	<div style="clear:both"></div>
+
+
+</form>
+
+ </div>
+ 	<div class="col-md-4 text-center como-llegar">
+	  <h4 style="text-align:left; font-size:22px; ">&iquest;C&oacute;mo llegar?</h4>
+
+<iframe style="width:100%" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7041.753410817005!2d-16.615605!3d28.058789!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc6a9f25bf18e9db%3A0xba77887e135a927b!2sCalle+Mencey+Anaga%2C+21%2C+38639%2C+Santa+Cruz+de+Tenerife%2C+Espa%C3%B1a!5e0!3m2!1ses!2ses!4v1496335755355" width="600" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+  </div>
+  </div>
+</section>
+<footer style="background:#333;padding:40px 0">
+<div class="container">
+<div class="row">
+<div class="col-md-4" style="text-align:center; padding:0px 0 10px 0">
+<img src="img/logo-bahia-los-abrigos.png" style="width:120px; padding:0px 0"/>
+</div>
+
+<div class="col-md-4" style="padding:0px 0 10px 0">
+<h4 style="color:white; text-align:center; font-size:26px; ">S&iacute;guenos</h4>
+<div style="color:white; font-size:18px; text-align:center">#RESTAURANTE</b></div>
+<div style="color:white; font-size:40px; text-align:center">
+<a href="http://www.facebook.com" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+<a href="http://www.twitter.com" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+<a href="http://www.instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+</div>
+</div>
+<div class="col-md-4">
+<!--<a href="https://api.whatsapp.com/send?phone=584142051422">
+prueba
+</a>-->
+<div style="color:white; font-size:16px; text-align:left;  padding:10px 0 10px 0">
+Restaurante Bahía Los Abrigos<br>
+Teléfono: <a href="tel:#">+34 922 74 98 56</a><br>
+Abierto de 12:30 a 23:30 horas todos los días excepto martes<br>
+Calle La Marina, 40 – Los Abrigos. Tenerife Sur
+</div>
+</div>
+</div>
+</div>
+</footer>
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<!-- Theme JavaScript -->
+<script src="js/creative.min.js"></script>
+<!--//BLOQUE COOKIES-->
+</body>
+</html>
